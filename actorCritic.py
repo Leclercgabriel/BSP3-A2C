72e98818dbaf7a0ce3a063e8f31d9dc07ec1ef50
Gabriel Leclerc <gabriel.leclerc001@student.uni.lu>
2023-11-25 16:48:10 +0100

    separated ac

--- actorCritic.py
+++ actorCritic.py
@@ -1,24 +1,55 @@
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 import tensorflow_probability as tfp
-from networks import actorCriticNetwork
+#from networks import actorCriticNetwork
 import numpy as np
+import os
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Model
 
-class Agent:
+class ActorCritic:
     def __init__(self, alpha=0.01, gamma=0.95, action_space=4, observation_space=8):
+        super(ActorCritic, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.action_space = action_space
         self.action = None
         self.actionSpace = [i for i in range (0, self.action_space)]
 
-        self.actorCritic = actorCriticNetwork(action_space=action_space)
+        self.actor_checkpoint_file = os.path.join("logs/actor")
+        self.critic_checkpoint_file = os.path.join("logs/critic")
 
-        self.actorCritic.compile(optimizer=Adam(learning_rate=alpha))
+        #self.aD1_dims = aD1_dims
+        #self.aD2_dims = aD2_dims
+        self.aOutput_dims = action_space
+
+        #self.cD1_dims = cD1_dims
+        #self.cD2_dims = acD2_dims
+        #self.cOutput_dims = 1
+
+        self.stateInput = Input(shape=(observation_space))
+
+        self.aD1 = Dense(100, activation="relu")(self.stateInput)
+        self.aD2 = Dense(50, activation="relu")(self.aD1)
+        self.aOutput = Dense(self.aOutput_dims, activation="softmax")(self.aD2)
+
+        self.cD1 = Dense(100, activation="relu")(self.stateInput)
+        self.cD2 = Dense(50, activation="relu")(self.cD1)
+        self.cOutput = Dense(1, activation=None)
+
+        self.Actor = Model(inputs=self.stateInput, outputs=self.aOutput)
+        self.Critic = Model(inputs=self.stateInput, outputs=self.cOutput)
+
+        self.Actor.summary()
+        self.Critic.summary()
+        
+        self.Actor.compile(optimizer=Adam(learning_rate=self.alpha))
+        self.Critic.compile(optimizer=Adam(learning_rate=self.alpha))
 
     def chooseAction(self, observations):
         state = tf.convert_to_tensor([observations])
-        _, proba = self.actorCritic(state)
+        proba = self.Actor(state)
 
         actionProba = tfp.distributions.Categorical(probs=proba)
         action = actionProba.sample()
@@ -26,31 +57,68 @@ class Agent:
 
         return action.numpy()[0]
 
+    def aSave(self):
+        print("...saving actor...")
+        self.Actor.save_weights(self.actor_checkpoint_file)
+
+    def aLoad(self):
+        print("...loading actor...")
+        self.Actor.load_weights(self.actor_checkpoint_file)
+    
+    def cSave(self):
+        print("...saving critic...")
+        self.Critic.save_weights(self.critic_checkpoint_file)
+
+    def cLoad(self):
+        print("...loading critic...")
+        self.Critic.load_weights(self.critic_checkpoint_file)
+
     def save(self):
         print("...saving...")
-        self.actorCritic.save_weights(self.actorCritic.checkpoint_file)
+        self.aSave()
+        self.cSave()
 
     def load(self):
         print("...loading...")
-        self.actorCritic.load_weights(self.actorCritic.checkpoint_file)
-    
+        self.aLoad()
+        self.cLoad()
+
     def learn(self, state, reward, state1, done):
         state = tf.convert_to_tensor([state], dtype=tf.float32)
         state1 = tf.convert_to_tensor([state1], dtype=tf.float32)
         reward = tf.convert_to_tensor(reward, dtype=tf.float32)
         with tf.GradientTape(persistent=True) as tape:
-            V_value, proba = self.actorCritic(state)
-            V_value1, _ = self.actorCritic(state1)
+            
+            actionProba = self.Actor(state)
+
+            V_value = self.Critic(state)
+            V_value1 = self.Critic(state1)
             V_value = tf.squeeze(V_value)
             V_value1 = tf.squeeze(V_value1)
 
-            actionProba = tfp.distributions.Categorical(probs=proba)
-            log_proba = actionProba.log_prob(self.action)
+            actionProba = tfp.distributions.Categorical(probs=actionProba)
+            logProba = actionProba.log_prob(self.action)
 
-            delta = reward + self.gamma*V_value1*(1-int(done)) - V_value
-            actorLoss = -log_proba*delta
-            criticLoss = delta**2
-            totalLoss = actorLoss + criticLoss
+            advantage = reward + self.gamma*V_value1*(1-int(done)) - V_value
 
-        gradient = tape.gradient(totalLoss, self.actorCritic.trainable_variables)
-        self.actorCritic.optimizer.apply_gradients(zip(gradient, self.actorCritic.trainable_variables))
+            aLoss = -logProba * advantage
+            cLoss = advantage ** 2
+
+        aGradient = tape.gradient(aLoss, self.Actor.trainable_variables)
+        cGradient = tape.gradient(cLoss, self.Critic.trainable_variables)
+        
+        self.Actor.optimizer.apply_gradients(zip(aGradient, self.Actor.trainable_variables))
+        self.Critic.optimizer.apply_gradients(zip(cGradient, self.Critic.trainable_variables))
+        
+        del tape
+
+
+
+
+
+
+
+
+
+
+
