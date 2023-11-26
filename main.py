31d5221dfa7e6a13b792088a260d2be178be86ea
Gabriel Leclerc <gabriel.leclerc001@student.uni.lu>
2023-11-26 11:55:18 +0100

    working hyperparameters

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from actorCritic import ActorCritic
 from utils import plot_learning_curve
 from datetime import datetime
 import atexit
+import sys
 
 if __name__ == "__main__":
     env = gym.make("LunarLander-v2")
-    agent = ActorCritic(alpha=1e-5, gamma=0.50, action_space=env.action_space.n)
-    n_games = 1000
+    agent = ActorCritic(alpha=1e-4, gamma=0.90, action_space=env.action_space.n)
+    n_games = 1500
 
-    filePath = "tmp/img/LunarLander-g"+str(agent.gamma)+"-a"+str(agent.alpha)+"-"+str(datetime.now())+".png"
+    filePath = "tmp/img/LunarLander"+"-"+str(datetime.now())+"-g"+str(agent.gamma)+"-a"+str(agent.alpha)+".png"
     figureFile = filePath
     highScore = env.reward_range[0]
     scoreHistory = []
@@ -31,13 +32,19 @@ if __name__ == "__main__":
                 if not loadCheckpoint:
                     agent.learn(observation, reward, observation1, done)
                 observation = observation1
-        except KeyboardInterrupt:
+        except (KeyboardInterrupt, AssertionError, ValueError):
+            print(action)
             x = [j+1 for j in range (i)]
             plot_learning_curve(x, scoreHistory, figureFile)
-        
+            sys.exit()
+
         scoreHistory.append(score)
         averageScore = np.mean(scoreHistory[-100:])
-        
+
+        if i % 100 == 0 and i >= 100:
+            x = [j+1 for j in range (i)]
+            plot_learning_curve(x, scoreHistory, figureFile)
+
         if averageScore > highScore:
             highScore = averageScore
             if not loadCheckpoint:
