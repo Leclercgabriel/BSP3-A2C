corrected precision and added number or neurons

actorCritic.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Model
 
 class ActorCritic:
-    def __init__(self, alpha=0.01, gamma=0.95, action_space=4, observation_space=8):
+    def __init__(self, alpha=0.01, gamma=0.99, action_space=4, observation_space=8, a1=64, a2=32, c1=64, c2=32):
         super(ActorCritic, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
@@ -20,23 +20,23 @@ class ActorCritic:
         self.actor_checkpoint_file = os.path.join("logs/actor")
         self.critic_checkpoint_file = os.path.join("logs/critic")
 
-        #self.aD1_dims = aD1_dims
-        #self.aD2_dims = aD2_dims
+        self.aD1_dims = a1
+        self.aD2_dims = a2
         self.aOutput_dims = action_space
 
-        #self.cD1_dims = cD1_dims
-        #self.cD2_dims = acD2_dims
-        #self.cOutput_dims = 1
+        self.cD1_dims = c1
+        self.cD2_dims = c2
+        self.cOutput_dims = 1
 
         self.stateInput = Input(shape=(observation_space))
 
-        self.aD1 = Dense(86, activation=tf.keras.layers.LeakyReLU())(self.stateInput)
-        self.aD2 = Dense(32, activation=tf.keras.layers.LeakyReLU())(self.aD1)
+        self.aD1 = Dense(self.aD1_dims, activation=tf.keras.layers.LeakyReLU())(self.stateInput)
+        self.aD2 = Dense(self.aD2_dims, activation=tf.keras.layers.LeakyReLU())(self.aD1)
         self.aOutput = Dense(self.aOutput_dims, activation="softmax")(self.aD2)
 
-        self.cD1 = Dense(86, activation=tf.keras.layers.LeakyReLU())(self.stateInput)
-        self.cD2 = Dense(32, activation=tf.keras.layers.LeakyReLU())(self.cD1)
-        self.cOutput = Dense(1, activation=None)(self.cD2)
+        self.cD1 = Dense(self.cD1_dims, activation=tf.keras.layers.LeakyReLU())(self.stateInput)
+        self.cD2 = Dense(self.cD2_dims, activation=tf.keras.layers.LeakyReLU())(self.cD1)
+        self.cOutput = Dense(self.cOutput_dims, activation=None)(self.cD2)
 
         self.Actor = Model(inputs=self.stateInput, outputs=self.aOutput)
         self.Critic = Model(inputs=self.stateInput, outputs=self.cOutput)
@@ -52,10 +52,10 @@ class ActorCritic:
         proba = self.Actor(state)
 
         actionProba = tfp.distributions.Categorical(probs=proba)
-        action = actionProba.sample()
+        action = actionProba.sample().numpy()[0]
         self.action = action
 
-        return action.numpy()[0]
+        return action, actionProba, proba
 
     def aSave(self):
         print("...saving actor...")
@@ -84,8 +84,8 @@ class ActorCritic:
         self.cLoad()
 
     def learn(self, state, reward, state1, done):
-        state = tf.convert_to_tensor([state], dtype=tf.float32)
-        state1 = tf.convert_to_tensor([state1], dtype=tf.float32)
+        state = tf.convert_to_tensor([state], dtype=tf.float64)
+        state1 = tf.convert_to_tensor([state1], dtype=tf.float64)
         reward = tf.convert_to_tensor(reward, dtype=tf.float32)
         with tf.GradientTape(persistent=True) as tape:
             
@@ -111,5 +111,4 @@ class ActorCritic:
         self.Critic.optimizer.apply_gradients(zip(cGradient, self.Critic.trainable_variables))
         
         del tape
-
 

main.py
@@ -8,10 +8,10 @@ import sys
 
 if __name__ == "__main__":
     env = gym.make("LunarLander-v2")
-    agent = ActorCritic(alpha=1e-4, gamma=0.90, action_space=env.action_space.n)
+    agent = ActorCritic(alpha=1e-4, gamma=0.95, action_space=env.action_space.n, a1=86, a2=64, c1=88, c2=32 )
     n_games = 1500
 
-    filePath = "tmp/img/LunarLander"+"-"+str(datetime.now())+"-g"+str(agent.gamma)+"-a"+str(agent.alpha)+".png"
+    filePath = "tmp/img/LunarLander"+"-"+str(datetime.now())+"-g"+str(agent.gamma)+"-a"+str(agent.alpha)+"a1"+str(agent.aD1_dims)+"a2"+str(agent.aD2_dims)+"c1"+str(agent.cD1_dims)+"c2"+str(agent.cD2_dims)+".png"
     figureFile = filePath
     highScore = env.reward_range[0]
     scoreHistory = []
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         score = 0
         try:
             while not done:
-                action = agent.chooseAction(observation)
+                action, actionProba, probs = agent.chooseAction(observation)
                 observation1, reward, done, _, info = env.step(action)
                 score += reward
                 if not loadCheckpoint:
@@ -34,16 +34,14 @@ if __name__ == "__main__":
                 observation = observation1
         except (KeyboardInterrupt, AssertionError, ValueError):
             print(action)
+            print(actionProba.sample())
+            print(probs)
             x = [j+1 for j in range (i)]
             plot_learning_curve(x, scoreHistory, figureFile)
             sys.exit()
 
         scoreHistory.append(score)
         averageScore = np.mean(scoreHistory[-100:])
-
-        if i % 100 == 0 and i >= 100:
-            x = [j+1 for j in range (i)]
-            plot_learning_curve(x, scoreHistory, figureFile)
 
         if averageScore > highScore:
             highScore = averageScore